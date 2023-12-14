a1431686d4ff8677e1e920b4e7873bbbfa5984ca
vipink28 <vipinkalyan28@gmail.com>
2023-12-14 10:31:05 +0530

    Browse page created

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import Header from '../components/Header';
+import { fetchNetflixOriginals, selectNetflixOriginals } from '../features/tv/tvSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchNowPlayingMovies, selectNowPlayingMovies } from '../features/movie/movieSlice';
 
 function Browse(props) {
     const param = useParams();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const collection = useSelector(param.platform === 'tv' ? selectNetflixOriginals : selectNowPlayingMovies);
+    const { data, error, status } = collection;
+
+
+    useEffect(() => {
+        if (param.platform === 'tv') {
+            dispatch(fetchNetflixOriginals());
+        } else if (param.platform === 'movie') {
+            dispatch(fetchNowPlayingMovies());
+        } else {
+            navigate('/browse/tv');
+        }
+    }, [param])
+
+    const random = Math.floor(Math.random() * data?.results.length);
 
     return (
         <>
-            <Header />
+            {
+                <Header video={data?.results[random]} platform={param.platform} />
+            }
         </>
     );
 }
